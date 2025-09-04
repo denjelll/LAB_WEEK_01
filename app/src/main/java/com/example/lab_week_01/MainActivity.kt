5e9a99c3c5b334e43c0205cc99fbc5936b2c83a5
denjelll <165755569+denjelll@users.noreply.github.com>
2025-09-04 17:06:03 +0700

    make an alert message

--- app/src/main/java/com/example/lab_week_01/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_01/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.lab_week_01
 
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val nameDisplay: TextView = findViewById(R.id.name_display)
+        val nameSubmit: Button = findViewById(R.id.name_submit)
+        val nameInputEditText: TextInputEditText = findViewById(R.id.name_input)
+        nameSubmit.setOnClickListener {
+            val nameInput = nameInputEditText.text.toString().trim()
+            if (nameInput.isNotEmpty()) {
+                nameDisplay.text = getString(R.string.name_greet).plus(" ").plus(nameInput)
+// or you can use
+// nameDisplay?.text = "${getString(R.string.name_greet)} ${nameInput}"
+            } else {
+                Toast.makeText(this, getString(R.string.name_empty), Toast.LENGTH_LONG)
+                    .apply {
+                        setGravity(Gravity.CENTER, 0, 0)
+                        show()
+                    }
+            }
         }
     }
 }
